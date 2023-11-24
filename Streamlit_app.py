68905b6b302bed4cf4102087afdfc8bdbc4a2bcc
Jwcbaw34 <130948433+Jwcbaw34@users.noreply.github.com>
2023-11-24 11:34:35 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+import requests 
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
@@ -14,4 +15,22 @@ from langchain import PromptTemplate
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 PASSWORD = "sgh"
 
-st.write('Hello world! testing 1 2 3 ')
+# Define the URL of your document on GitHub
+github_document_url = "https://raw.githubusercontent.com/Jwcbaw34/KM/main/20161027%20MICU%20Medical%20Board%20Paper%20(1%20Nov%202016%20MB%20Meeting).pdf"
+
+# Check if the document is already downloaded; if not, download it
+local_document_path = "20161027 MICU Medical Board Paper (1 Nov 2016 MB Meeting).pdf"
+
+if not os.path.exists(local_document_path):
+    st.info("Downloading document...")
+    response = requests.get(github_document_url)
+
+    if response.status_code == 200:
+        with open(local_document_path, "wb") as file:
+            file.write(response.content)
+        st.success("Document downloaded successfully.")
+    else:
+        st.error(f"Failed to download document: {response.status_code}")
+else:
+    st.success("Document already downloaded.")
+
