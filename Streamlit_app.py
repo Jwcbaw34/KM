6866acdb4a344df1bb2a47bd4e92313ef06959f5
Jwcbaw34 <130948433+Jwcbaw34@users.noreply.github.com>
2023-11-24 11:02:46 +0800

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,14 +1,7 @@
 import os
 import streamlit as st
-from langchain.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import RetrievalQA
 from htmlTemplates import css, bot_template, user_template
-from langchain import PromptTemplate
+
 
 #os.environ["OPENAI_API_KEY"] = "abc"
 openai_api_key = os.environ.get("OPENAI_API_KEY")
